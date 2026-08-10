024a1d3214f29564e329e0ec63c5ab689353f3dd
Ayush Sonkar <meayushsonkar@gmail.com>
2026-08-10 20:11:23 +0530

    Time: 3 ms (40.60%) | Memory: 58.3 MB (14.45%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length
+    let expectedsum = n * (n + 1) / 2
+    let actualsum = 0;
+
+    for (let num of nums) {
+        actualsum += num;
+    }
+
+    return expectedsum - actualsum
+};
