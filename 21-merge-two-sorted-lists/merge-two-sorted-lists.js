dbd889e2b4f0330647171ffb00f61fdf5e24630c
Ayush Sonkar <meayushsonkar@gmail.com>
2026-08-10 19:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (27.19%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if(!l1 || !l2) {
+        return l1 || l2
+    }
+
+    if ( l1.val > l2.val) {
+        [l2,l1] = [l1,l2]
+    }
+
+    l1.next = mergeTwoLists(l1.next, l2)
+
+    return l1
+};
