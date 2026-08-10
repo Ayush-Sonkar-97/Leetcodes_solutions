f0f7c038f52ff8192200806f8448e0ca52cdb4e4
Ayush Sonkar <meayushsonkar@gmail.com>
2026-08-10 20:04:19 +0530

    Time: 4 ms (32.80%) | Memory: 65.7 MB (19.81%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0], max = 0
+
+    for(let i = 0; i < prices.length; i++) {
+        max = Math.max(prices[i] - min, max)
+        min = Math.min(min, prices[i])
+    }
+
+    return max
+};
