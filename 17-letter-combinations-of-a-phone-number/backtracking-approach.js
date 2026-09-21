45e702cd8844414d1cdd9a5fc3e9e90dd17511d4
Ayush Sonkar <meayushsonkar@gmail.com>
2026-09-21 17:11:16 +0530

    Add comment for backtracking approach in function
    Added a comment indicating the use of backtracking approach.

---
+++ 17-letter-combinations-of-a-phone-number/backtracking-approach.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+// used backtracking approach
+var letterCombinations = function(digits) {
+    const len = digits.length
+
+    if(len === 0) return []
+
+    const phone_map = ["abc", "def", "ghi", 'jkl', "mno", "pqrs", "tuv", "wxyz"];
+
+    const output = []
+
+    const backtrack = (combination, next_digit, phone_map, output) => {
+        if(next_digit.length === 0) {
+            output.push(combination)
+            // console.log(output)
+        }
+        else {
+            const new_digit = next_digit[0] - '2'
+            const letters = phone_map[new_digit]
+            for(let i = 0; i < letters.length; i++) {
+                backtrack(combination + letters[i], next_digit.slice(1), phone_map, output)
+            }
+        }
+    }
+
+    backtrack("", digits, phone_map, output)
+
+    return output
+
+}
