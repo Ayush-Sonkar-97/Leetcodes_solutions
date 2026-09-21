a7ca82fa83c499640efa36aa834c8fa9829f6308
Ayush Sonkar <meayushsonkar@gmail.com>
2026-09-22 00:17:25 +0530

    Time: 65 ms (34.18%) | Memory: 53.5 MB (61.77%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var fib = function(n) {
-    let ans = [0, 1]
-    // console.log(ans)
-    for(let i = 2; i <= n; i++) {
-        ans.push(ans[i - 1] + ans[i - 2])
-        console.log(ans)
-    }
+    // let ans = [0, 1]
+    // // console.log(ans)
+    // for(let i = 2; i <= n; i++) {
+    //     ans.push(ans[i - 1] + ans[i - 2])
+    //     console.log(ans)
+    // }
 
-    return ans[n]
+    // return ans[n]
+
+    if(n == 1) return 1
+    if (n == 0) return 0
+
+    return fib(n-1) + fib(n-2)
 };
