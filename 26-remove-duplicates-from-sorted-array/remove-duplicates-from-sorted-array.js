b436e7277263acc1f0321ab7934df47a11612004
Ayush Sonkar <meayushsonkar@gmail.com>
2026-09-22 19:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 56.8 MB (84.31%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,13 +3,26 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let i = 0
+    // let i = 0
 
-    for (let j = 0; j < nums.length; j++) {
-        if(nums[i] !== nums[j]) {
-            nums[++i] = nums[j]
+    // for (let j = 0; j < nums.length; j++) {
+    //     if(nums[i] !== nums[j]) {
+    //         nums[++i] = nums[j]
+    //     }
+    // }
+
+    // return ++i
+
+    if(!nums) return 0
+
+    let slow = 1
+
+    for(let fast = 1; fast < nums.length; fast++) {
+        if(nums[fast] !== nums[slow - 1]) {
+            nums[slow] = nums[fast]
+            slow += 1
         }
     }
 
-    return ++i
+    return slow
 };
