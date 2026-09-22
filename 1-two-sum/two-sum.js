e91600dc82fe2e65070a44d4d612b1b8d67d8031
Ayush Sonkar <meayushsonkar@gmail.com>
2026-09-22 20:28:13 +0530

    Time: 5 ms (46.81%) | Memory: 56.7 MB (12.52%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    // Two sum problem using brute force
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
-        }
-    }
-    return []
+    // // Two sum problem using brute force
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+    // return []
 
 
     // // using Hash Map
@@ -30,4 +30,29 @@ var twoSum = function (nums, target) {
 
     // }
     // return []
+
+    // Using two pointer approach
+    const indexedNums = nums.map((val, idx) => ({ val, idx }))
+    // console.log(indexedNums)
+
+    const sorted = indexedNums.sort((a, b) => a.val - b.val)
+    // console.log(sorted)
+
+    let left = 0;
+    let right = nums.length - 1
+
+    while (left < right) {
+        const sum = indexedNums[left].val + indexedNums[right].val
+
+        if (sum === target) {
+            return [indexedNums[left].idx, indexedNums[right].idx]
+        }
+        else if (sum < target) {
+            left += 1
+        }
+        else {
+            right -= 1
+        }
+    }
+    return []
 };
