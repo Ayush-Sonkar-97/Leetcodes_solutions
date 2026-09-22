20712e4e25039db3700b330b9a16ef931416b5fe
Ayush Sonkar <meayushsonkar@gmail.com>
2026-09-22 20:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (41.23%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,16 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const map = new Map();
+var twoSum = function (nums, target) {
+    // // Two sum problem using brute force
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+    // return []
+    // using Hash Map
+    let visitedMap = new Map()
 
-    for(let i =0; i < nums.length; i++){
-        const diff = target - nums[i]
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+        // console.log(`value = ${nums[i]}, index = ${i}, complement = ${target} - ${nums[i]} = ${complement}`)
 
-        if(map.has(diff)) {
-            return [map.get(diff) ,i];
+        if (visitedMap.has(complement)) {
+            return [visitedMap.get(complement), i]
         }
 
-        map.set(nums[i], i)
+        visitedMap.set(nums[i], i)
+
     }
+    return []
 };
