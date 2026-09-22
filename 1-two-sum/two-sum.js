3f1d9c66e01896eab9a1480208d0f288cb44652e
Ayush Sonkar <meayushsonkar@gmail.com>
2026-09-22 20:15:29 +0530

    Time: 40 ms (16.82%) | Memory: 54.2 MB (78.00%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,28 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    // // Two sum problem using brute force
-    // for (let i = 0; i < nums.length; i++) {
-    //     for (let j = i + 1; j < nums.length; j++) {
-    //         if (nums[i] + nums[j] === target) {
-    //             return [i, j]
-    //         }
-    //     }
-    // }
-    // return []
-    // using Hash Map
-    let visitedMap = new Map()
-
+    // Two sum problem using brute force
     for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i]
-        // console.log(`value = ${nums[i]}, index = ${i}, complement = ${target} - ${nums[i]} = ${complement}`)
-
-        if (visitedMap.has(complement)) {
-            return [visitedMap.get(complement), i]
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
         }
-
-        visitedMap.set(nums[i], i)
-
     }
     return []
+
+
+    // // using Hash Map
+    // let visitedMap = new Map()
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     let complement = target - nums[i]
+    //     // console.log(`value = ${nums[i]}, index = ${i}, complement = ${target} - ${nums[i]} = ${complement}`)
+
+    //     if (visitedMap.has(complement)) {
+    //         return [visitedMap.get(complement), i]
+    //     }
+
+    //     visitedMap.set(nums[i], i)
+
+    // }
+    // return []
 };
